3233e117c4ba76aac1bcb32fb5fb0850380bf010
jayoung977 <wjsrnr20@gmail.com>
2023-05-03 14:07:50 +0900

    make: init folders

---
+++ server/app.js
@@ -0,0 +1,17 @@
+// 예시 코드
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 8080;
+
+//요청한 곳의 포트번호가 다르더라도 허용
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const todoRouter = require("./routes/todo");
+app.use("/api", todoRouter);
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
---
+++ server/models/index.js
@@ -0,0 +1,18 @@
+// 예시 코드
+const Sequelize = require("sequelize");
+const config = require(__dirname + "/../config/config.json")["development"];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+// TODO: model 연결
+db.Todo = require("./Todo")(sequelize, Sequelize);
+module.exports = db;
