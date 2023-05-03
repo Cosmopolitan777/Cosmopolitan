a2e82256f00b863bc05a9cecb9320e3fe363cc71
jayoung977 <wjsrnr20@gmail.com>
2023-05-03 20:25:14 +0900

    design: React-Slick carousel 기능 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import CocktailDetail from "./Components/CocktailDetail";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CocktailDetail />
+    </div>
+  );
 }
 
 export default App;
