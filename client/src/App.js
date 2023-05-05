f155d90cb6ebda8544e01b14fd2fc263f5dc9806
jayoung977 <wjsrnr20@gmail.com>
2023-05-05 18:49:07 +0900

    edit app.js connect db

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
+import {useState, useEffect} from "react";
 // import CocktailItem from "./components/CocktailItem";
 import CocktailDetail from "./pages/CocktailDetail";
+import axios from "axios";
+import {API_BASE_URL} from "./app-config";
 function App() {
+  const [cocktailItems, setCocktailItems] = useState([]);
+  useEffect(() => {
+    console.log("mount 완료");
+    const getCocktails = async () => {
+      const res = await axios.get(`${API_BASE_URL}/cocktail/showlist`);
+      setCocktailItems(res.data);
+      console.log("res.data", res.data);
+    };
+    getCocktails();
+  }, []);
   return (
     <div className="App">
       <CocktailDetail />
