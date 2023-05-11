f75fb3e2dd3c1f09da585b5a7e0cf261aef7f6af
ji-dawn <wlgy33@naver.com>
2023-05-12 01:43:17 +0900

    make: 게시글 작성 컴포넌트 생성

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -1,32 +1,38 @@
-import {useRef, useEffect} from "react";
-import Quill from "quill";
-import "quill/dist/quill.bubble.css";
+import React, {useState, useRef} from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import "../styles/Editor.scss";
 
 const Editor = () => {
-  const quillElement = useRef(null); // Quill을 적용할 div 설정
-  const quillInstance = useRef(null); // Quill 인스턴스 설정
+  const quillRef = useRef(); // Quill을 적용할 div 설정
+  const quillInstance = useRef(); // Quill 인스턴스 설정
 
-  useEffect(() => {
-    quillInstance.current = new Quill(quillElement.current, {
-      theme: "bubble",
-      placeholder: "내용을 작성하세요",
-      modules: {
-        toolbar: [
-          [{header: "1"}, {header: "2"}],
-          ["bold", "italic", "underline", "strike"],
-          [{list: "ordered"}, {list: "bullet"}],
-          ["blockquote", "link"],
-        ],
-      },
-    });
-  }, []);
+  const [content, setContent] = useState("");
+
+  const modules = {
+    toolbar: [
+      //[{ 'font': [] }],
+      [{header: [1, 2, false]}],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{list: "ordered"}, {list: "bullet"}],
+      ["link"],
+      [{align: []}, {color: []}, {background: []}],
+      ["clean"],
+    ],
+  };
 
   return (
     <div className="editor-block">
       <input className="editor-input-title" placeholder="제목을 입력하세요" />
       <div className="editor-quill-container">
-        <div ref={quillElement} />
+        <ReactQuill
+          ref={quillRef}
+          modules={modules}
+          theme="snow"
+          placeholder="내용을 입력해주세요"
+          value={content}
+          onChange={setContent}
+        />
       </div>
     </div>
   );
