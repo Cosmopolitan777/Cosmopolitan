58632887ad4a5f1683e800aaf32c358303368e09
coing-ye <lgtwin144@gmail.com>
2023-05-12 19:23:18 +0900

    Update: 게시글 작성 router 변경

--- server/routes/community.js
+++ server/routes/community.js
@@ -11,18 +11,18 @@ router.get("/", controller.main);
 router.post("/tc", controller.create);
 
 // 게시글 조회
-router.get("/tr", controller.read);
+router.post("/tr", controller.read);
 
 //게시글 수정
-router.patch("/tu", controller.update);
+router.post("/tu", controller.update);
 
 //게시글 삭제
-router.delete("/td", controller.delete);
+router.post("/td", controller.delete);
 
 //특정 게시글 조회
 router.get("/st/:textidx",controller.showtext);
 
 //본인 작성 글 목록 조회
-router.get("/mt",controller.mytext);
+router.post("/mt",controller.mytext);
 
 module.exports = router;
