8be957ac3709523baeb65ca8b77d3a1c89a8e8a7
ji-dawn <wlgy33@naver.com>
2023-05-13 17:24:00 +0900

    update: 게시판 Read, Delete 요청 수정

--- server/routes/community.js
+++ server/routes/community.js
@@ -3,7 +3,6 @@ const controller = require("../controller/Ccommunity");
 // const router = require('../routes');
 const router = express.Router();
 
-
 //메인화면
 router.get("/", controller.main);
 
@@ -11,18 +10,18 @@ router.get("/", controller.main);
 router.post("/tc", controller.create);
 
 // 게시글 조회
-router.post("/tr", controller.read);
+router.get("/tr", controller.read);
 
 //게시글 수정
 router.post("/tu", controller.update);
 
 //게시글 삭제
-router.post("/td", controller.delete);
+router.delete("/td", controller.delete);
 
 //특정 게시글 조회
-router.get("/st/:textidx",controller.showtext);
+router.get("/st/:textidx", controller.showtext);
 
 //본인 작성 글 목록 조회
-router.post("/mt",controller.mytext);
+router.post("/mt", controller.mytext);
 
 module.exports = router;
